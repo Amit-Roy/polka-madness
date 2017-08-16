960e78094d04ac4adebbaa513f9e61f6caa46c40
Amit-Roy <a.roy.0593@gmail.com>
2017-08-16 18:28:37 +0530

    added refresh

--- src/main/webapp/sketch.js
+++ src/main/webapp/sketch.js
@@ -7,31 +7,38 @@ function setup() {
 }
 
 function draw() {
-    counter++;
-    counter = counter % 7;
-    switch (counter) {
-        case 0:
-            fill(255, 0, 0);
-            break;
-        case 1:
-            fill(255, 127, 0);
-            break;
-        case 2:
-            fill(255, 255, 0);
-            break;
-        case 3:
-            fill(0, 255, 0);
-            break;
-        case 4:
-            fill(0, 0, 255);
-            break;
-        case 5:
-            fill(75, 0, 130);
-            break;
-        case 6:
-            fill(139, 0, 255);
-            break;
+    if (!mouseIsPressed) {
+        counter++;
+        counter = counter % 7;
+        switch (counter) {
+            case 0:
+                fill(255, 0, 0);
+                break;
+            case 1:
+                fill(255, 127, 0);
+                break;
+            case 2:
+                fill(255, 255, 0);
+                break;
+            case 3:
+                fill(0, 255, 0);
+                break;
+            case 4:
+                fill(0, 0, 255);
+                break;
+            case 5:
+                fill(75, 0, 130);
+                break;
+            case 6:
+                fill(139, 0, 255);
+                break;
+        }
+        ellipse(mouseX, mouseY, 60, 60);
     }
+}
 
-    ellipse(mouseX, mouseY, 60, 60);
+function keyPressed() {
+    if (keyCode === 82 || keyCode === 32 || keyCode === 67 || keyCode === 8) {
+        background(0);
+    }
 }
